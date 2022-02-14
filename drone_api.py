13ee1c5ace5a19fc86d56eab3c6782187fb86174
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-02-14 18:33:09 +0300

    Rewrote drone_api

--- drone_api.py
+++ drone_api.py
@@ -4,105 +4,124 @@ from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, SetMode
 from tf.transformations import quaternion_from_euler
 from threading import Thread
+from math import pi
 
 
 class Drone_api:
     def __init__(self):
-        self.started = False
+        self.__started = False
+        self.__type_of_move = None  # 'POSE' or 'SPEED'
+
         self.current_state = State()
         self.current_pose = PoseStamped()
         self.last_command_pose = None
 
-        self.thread_state = None
-        self.thread_pose = None
+        self.__thread_state = None
+        self.__thread_move = None
 
-        rospy.init_node('drone_offb', anonymous=True)
-        # Подписчик состояния, издатель локальной позиции, клиенты для смены режима и арма
-        self.state_sub = rospy.Subscriber('mavros/state', State,
-                                          self.state_cb, queue_size=10)
-        self.local_pos_sub = rospy.Subscriber('/mavros/local_position/pose',
-                                              PoseStamped, self.local_pos_cb, queue_size=10)
-        self.local_pos_pub = rospy.Publisher('mavros/setpoint_position/local',
-                                             PoseStamped, queue_size=10)
-        self.set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
-        self.arming_client = rospy.ServiceProxy('mavros/cmd/arming',
-                                                CommandBool)
+        rospy.init_node('drone_offb', anonymous=True, disable_signals=True)
+        # get state, position, set position, speeds, mode and arm
+        self.__state_sub = rospy.Subscriber('mavros/state', State,
+                                            self.__state_cb, queue_size=10)
+        self.__local_pos_sub = rospy.Subscriber('/mavros/local_position/pose',
+                                                PoseStamped, self.__local_pos_cb, queue_size=10)
+        self.__local_pos_pub = rospy.Publisher('mavros/setpoint_position/local',
+                                               PoseStamped, queue_size=10)
+        self.__set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
+        self.__arming_client = rospy.ServiceProxy('mavros/cmd/arming',
+                                                  CommandBool)
 
-    def state_cb(self, state):
+    def __del__(self):
+        self.finish()
+        # fixme: нужно, но в терминале выдает простыню
+        #rospy.signal_shutdown('Deleting an object Drone_api')
+
+    def __state_cb(self, state):
         self.current_state = state
 
-    def local_pos_cb(self, pose):
+    def __local_pos_cb(self, pose):
         self.current_pose = pose
 
-    def state_target(self):
+    def __state_target(self):
         rate = rospy.Rate(1)
-        """ prev_state = self.current_state """
-        while not rospy.is_shutdown():
+        while self.__started and not rospy.is_shutdown():
             try:
                 if self.current_state.mode != 'OFFBOARD':
-                    self.set_mode_client(base_mode=0, custom_mode='OFFBOARD')
+                    self.__set_mode_client(base_mode=0,
+                                           custom_mode='OFFBOARD')
                 else:
                     if not self.current_state.armed:
-                        self.arming_client(True)
-                """ if prev_state.armed != self.current_state.armed:
-                    self.arm = True """
-                """ prev_state = self.current_state """
+                        # fixme: дрон бесконечно дизармится, если не взлететь
+                        self.__arming_client(True)
                 try:
                     rate.sleep()
-                except rospy.exceptions.ROSTimeMovedBackwardsException as err:
+                except rospy.exceptions.ROSTimeMovedBackwardsException:
                     pass
             except rospy.ROSInterruptException:
                 pass
 
-    def pose_target(self):
-        # Установка скорости публикации выше 2Гц
+    def __move_target(self):
+        # The setpoint publishing rate MUST be faster than 2Hz
         rate = rospy.Rate(20)
-
-        while not rospy.is_shutdown():
+        while self.__started and not rospy.is_shutdown():
             try:
-                if self.last_command_pose is not None:
-                    self.local_pos_pub.publish(self.last_command_pose)
+                if self.__type_of_move == "POSE":
+                    self.__local_pos_pub.publish(self.last_command_pose)
+                elif self.__type_of_move == "SPEED":
+                    pass  # todo: Добавить управление скоростью
                 try:
                     rate.sleep()
-                except rospy.exceptions.ROSTimeMovedBackwardsException as err:
+                except rospy.exceptions.ROSTimeMovedBackwardsException:
                     pass
             except rospy.ROSInterruptException:
                 pass
 
-    # TODO: сделать приватной и сделать проверку started в set pose?
     def start(self):
-        if not self.started:
-            self.started = True
+        if not self.__started:
+            self.__started = True
+            self.__type_of_move = 'POSE'
+            self.last_command_pose = PoseStamped()
+            self.last_command_pose.pose.position.z = -3  # started but didn't takeoff
             rate = rospy.Rate(20)
-            # Ожидаем связь между МАВРОС и автопилотом
+            # Waiting for communication between MAVROS and autopilot
             while not rospy.is_shutdown() and not self.current_state.connected:
                 try:
                     rate.sleep()
-                except rospy.exceptions.ROSTimeMovedBackwardsException as err:
+                except rospy.exceptions.ROSTimeMovedBackwardsException:
                     pass
-            if self.thread_state is None:
-                thread_state = Thread(target=self.state_target, daemon=True)
-                thread_state.start()
-            if self.thread_pose is None:
-                thread_pose = Thread(target=self.pose_target, daemon=True)
-                thread_pose.start()
+            if self.__thread_state is None or not self.__thread_state.is_alive():
+                self.__thread_state = Thread(target=self.__state_target,
+                                             daemon=True)
+                self.__thread_state.start()
+            if self.__thread_move is None or not self.__thread_move.is_alive():
+                self.__thread_move = Thread(target=self.__move_target,
+                                            daemon=True)
+                self.__thread_move.start()
+
+    def finish(self):
+        if self.__started:
+            self.__started = False
+        rate = rospy.Rate(1)
 
     def set_pose(self, x=None, y=None, z=None, yaw=None):
-        self.start()
         new_pose = PoseStamped()
         if x is None:
-            x = self.current_pose.pose.position.x
+            x = self.last_command_pose.pose.position.x
         if y is None:
-            y = self.current_pose.pose.position.y
+            y = self.last_command_pose.pose.position.y
         if z is None:
-            z = self.current_pose.pose.position.z
+            z = self.last_command_pose.pose.position.z
         new_pose.pose.position.x = x
         new_pose.pose.position.y = y
         new_pose.pose.position.z = z
-        if yaw is not None:
+        if yaw is None:
+            new_pose.pose.orientation = self.last_command_pose.pose.orientation
+        else:
+            yaw = -yaw * (pi/180)
             q = quaternion_from_euler(0, 0, yaw)
             new_pose.pose.orientation.x = q[0]
             new_pose.pose.orientation.y = q[1]
             new_pose.pose.orientation.z = q[2]
             new_pose.pose.orientation.w = q[3]
         self.last_command_pose = new_pose
+        self.__type_of_move = "POSE"
--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from drone_api import Drone_api
 
-drone = Drone_api()
-drone.start()
-while True:
-    x = float(input('Введите x:'))
-    y = float(input('Введите y:'))
-    z = float(input('Введите z:'))
-    yaw = float(input('Введите yaw:'))
-    drone.set_pose(x, y, z, yaw)
-    print()
+try:
+    drone = Drone_api()
+    drone.start()
+    for i in range(2):
+        x = float(input('Введите x:'))
+        y = float(input('Введите y:'))
+        z = float(input('Введите z:'))
+        yaw = float(input('Введите yaw:'))
+        drone.set_pose(x, y, z, yaw)
+        print()
+    drone.finish()
+except KeyboardInterrupt:
+    pass
