03f5839163d37ac6f6f80edbacf8b3828aa9b92f
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-03-16 18:15:53 +0300

    Added global pose control

--- drone_api.py
+++ drone_api.py
@@ -1,6 +1,8 @@
-import posixpath
+from re import A
 import rospy
 from geometry_msgs.msg import PoseStamped, Twist
+from geographic_msgs.msg import GeoPoseStamped
+from sensor_msgs.msg import NavSatFix
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, SetMode
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
@@ -12,18 +14,24 @@ class Drone_api:
     def __init__(self):
         # GENERAL attributes
         self.__started = False
-        self.__type_of_move = None  # 'LOCAL_POSE' or 'VELOCITY'
+        self.__type_of_move = None  # 'LOCAL_POSE' or 'GLOBAL_POSE' or 'VELOCITY'
         self.__current_state = State()
-        self.__current_pose = PoseStamped()
+        self.__current_local_pose = PoseStamped()
+        self.__current_global_pose = NavSatFix()
         self.__thread_command = None
+        self.__yaw_head_first = False
 
         # LOCAL_POSE attributes
         self.__allowable_error = 0.5  # m
-        self.__last_command_pose = None
-        self.__yaw_head_first = False
+        self.__last_command_local_pose = PoseStamped()
+        self.__last_command_local_pose.pose.orientation.w = 1  # correct quaternion
+
+        # GLOBAL_POSE attributes
+        self.__last_command_global_pose = GeoPoseStamped()
+        self.__last_command_global_pose.pose.orientation.w = 1
 
         # VELOCITY attributes
-        self.__last_command_vel = None
+        self.__last_command_vel = State()
 
         rospy.init_node('drone_offb', anonymous=True)
         # get state, position, set position, velocity, mode and arm
@@ -31,8 +39,12 @@ class Drone_api:
                                             self.__state_cb, queue_size=10)
         self.__local_pos_sub = rospy.Subscriber('/mavros/local_position/pose',
                                                 PoseStamped, self.__local_pos_cb, queue_size=10)
+        self.__global_pos_sub = rospy.Subscriber('/mavros/global_position/global',
+                                                 NavSatFix, self.__global_pos_cb, queue_size=10)
         self.__local_pos_pub = rospy.Publisher('mavros/setpoint_position/local',
                                                PoseStamped, queue_size=10)
+        self.__global_pos_pub = rospy.Publisher('mavros/setpoint_position/global',
+                                                GeoPoseStamped, queue_size=10)
         self.__local_vel_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel_unstamped',
                                                Twist, queue_size=10)
         self.__set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
@@ -43,7 +55,10 @@ class Drone_api:
         self.__current_state = state
 
     def __local_pos_cb(self, pose):
-        self.__current_pose = pose
+        self.__current_local_pose = pose
+
+    def __global_pos_cb(self, pose):
+        self.__current_global_pose = pose
 
     def __command_target(self):
         rate = rospy.Rate(20)
@@ -68,19 +83,35 @@ class Drone_api:
                         self.__arming_client(True)
                 # CONTROL
                 if self.__type_of_move == 'LOCAL_POSE':
-                    delta_x = (self.__last_command_pose.pose.position.x
-                               - self.__current_pose.pose.position.x)
-                    delta_y = (self.__last_command_pose.pose.position.y
-                               - self.__current_pose.pose.position.y)
+                    delta_x = (self.__last_command_local_pose.pose.position.x
+                               - self.__current_local_pose.pose.position.x)
+                    delta_y = (self.__last_command_local_pose.pose.position.y
+                               - self.__current_local_pose.pose.position.y)
                     distance = sqrt((delta_x)**2 + (delta_y)**2)
                     if self.__yaw_head_first and distance > self.__allowable_error:
                         yaw = atan2(delta_y, delta_x)
                         q = quaternion_from_euler(0, 0, yaw)
-                        self.__last_command_pose.pose.orientation.x = q[0]
-                        self.__last_command_pose.pose.orientation.y = q[1]
-                        self.__last_command_pose.pose.orientation.z = q[2]
-                        self.__last_command_pose.pose.orientation.w = q[3]
-                    self.__local_pos_pub.publish(self.__last_command_pose)
+                        self.__last_command_local_pose.pose.orientation.x = q[0]
+                        self.__last_command_local_pose.pose.orientation.y = q[1]
+                        self.__last_command_local_pose.pose.orientation.z = q[2]
+                        self.__last_command_local_pose.pose.orientation.w = q[3]
+                    self.__local_pos_pub.publish(
+                        self.__last_command_local_pose)
+                elif self.__type_of_move == 'GLOBAL_POSE':
+                    delta_x = (self.__last_command_local_pose.pose.position.x
+                               - self.__current_local_pose.pose.position.x)
+                    delta_y = (self.__last_command_local_pose.pose.position.y
+                               - self.__current_local_pose.pose.position.y)
+                    distance = sqrt((delta_x)**2 + (delta_y)**2)
+                    if self.__yaw_head_first and distance > self.__allowable_error:
+                        yaw = atan2(delta_y, delta_x)
+                        q = quaternion_from_euler(0, 0, yaw)
+                        self.__last_command_global_pose.pose.orientation.x = q[0]
+                        self.__last_command_global_pose.pose.orientation.y = q[1]
+                        self.__last_command_global_pose.pose.orientation.z = q[2]
+                        self.__last_command_global_pose.pose.orientation.w = q[3]
+                    self.__global_pos_pub.publish(
+                        self.__last_command_global_pose)
                 elif self.__type_of_move == 'VELOCITY':
                     self.__local_vel_pub.publish(self.__last_command_vel)
                 rate.sleep()
@@ -92,8 +123,6 @@ class Drone_api:
         if not self.__started:
             self.__started = True
             self.__type_of_move = 'LOCAL_POSE'
-            self.__last_command_pose = PoseStamped()
-            self.__last_command_pose.pose.position.z = -3  # started but didn't takeoff
 
             if self.__thread_command is None or not self.__thread_command.is_alive():
                 self.__thread_command = Thread(target=self.__command_target,
@@ -123,42 +152,43 @@ class Drone_api:
         self.__allowable_error = distance
 
     def get_local_pose(self):
-        x = self.__current_pose.pose.position.x
-        y = self.__current_pose.pose.position.y
-        z = self.__current_pose.pose.position.z
-        angels = euler_from_quaternion([self.__current_pose.pose.orientation.x,
-                                        self.__current_pose.pose.orientation.y,
-                                        self.__current_pose.pose.orientation.z,
-                                        self.__current_pose.pose.orientation.w])
+        x = self.__current_local_pose.pose.position.x
+        y = self.__current_local_pose.pose.position.y
+        z = self.__current_local_pose.pose.position.z
+        angels = euler_from_quaternion([self.__current_local_pose.pose.orientation.x,
+                                        self.__current_local_pose.pose.orientation.y,
+                                        self.__current_local_pose.pose.orientation.z,
+                                        self.__current_local_pose.pose.orientation.w])
         roll = angels[0]
         pitch = angels[1]
         yaw = angels[2]
         return x, y, z, roll, pitch, yaw
 
-    def set_local_pose(self, x: float = None, y: float = None, z: float = None, yaw: float = None, yaw_head_first: bool = False):
+    def set_local_pose(self, x: float = None, y: float = None, z: float = None,
+                       yaw: float = None, yaw_head_first: bool = False):
         if self.__started == False:
             raise Exception('Drone is not running. '
                             'Please run \'<object_name>.start()\'')
         new_pose = PoseStamped()
         if x is None:
-            x = self.__last_command_pose.pose.position.x
+            x = self.__last_command_local_pose.pose.position.x
         if y is None:
-            y = self.__last_command_pose.pose.position.y
+            y = self.__last_command_local_pose.pose.position.y
         if z is None:
-            z = self.__last_command_pose.pose.position.z
+            z = self.__last_command_local_pose.pose.position.z
         new_pose.pose.position.x = x
         new_pose.pose.position.y = y
         new_pose.pose.position.z = z
         if yaw is None:
-            new_pose.pose.orientation = self.__last_command_pose.pose.orientation
+            new_pose.pose.orientation = self.__last_command_local_pose.pose.orientation
         else:
-            #yaw_rad = -yaw * (pi/180)
             q = quaternion_from_euler(0, 0, yaw)
             new_pose.pose.orientation.x = q[0]
             new_pose.pose.orientation.y = q[1]
             new_pose.pose.orientation.z = q[2]
             new_pose.pose.orientation.w = q[3]
-        self.__last_command_pose = new_pose
+        print(new_pose.pose.orientation)
+        self.__last_command_local_pose = new_pose
         self.__yaw_head_first = yaw_head_first
         self.__type_of_move = 'LOCAL_POSE'
 
@@ -166,17 +196,52 @@ class Drone_api:
         if self.__started == False:
             raise Exception('Drone is not running. '
                             'Please run \'<object_name>.start()\'')
-        delta_x = (self.__last_command_pose.pose.position.x
-                   - self.__current_pose.pose.position.x)
-        delta_y = (self.__last_command_pose.pose.position.y
-                   - self.__current_pose.pose.position.y)
-        delta_z = (self.__last_command_pose.pose.position.z
-                   - self.__current_pose.pose.position.z)
+        delta_x = (self.__last_command_local_pose.pose.position.x
+                   - self.__current_local_pose.pose.position.x)
+        delta_y = (self.__last_command_local_pose.pose.position.y
+                   - self.__current_local_pose.pose.position.y)
+        delta_z = (self.__last_command_local_pose.pose.position.z
+                   - self.__current_local_pose.pose.position.z)
         distance = sqrt((delta_x)**2 + (delta_y)**2 + (delta_z)**2)
         if distance < self.__allowable_error:
             return True
         else:
             return False
+
+    # GLOBAL_POSE methods
+    def get_global_pose(self):
+        latitude = self.__current_global_pose.latitude
+        longitude = self.__current_global_pose.longitude
+        altitude = self.__current_global_pose.altitude
+        return latitude, longitude, altitude
+
+    def set_global_pose(self, latitude: float = None, longitude: float = None, altitude: float = None,
+                        yaw: float = None, yaw_head_first: bool = False):
+        if self.__started == False:
+            raise Exception('Drone is not running. '
+                            'Please run \'<object_name>.start()\'')
+        new_pose = GeoPoseStamped()
+        if latitude is None:
+            latitude = self.__last_command_global_pose.latitude
+        if longitude is None:
+            longitude = self.__last_command_global_pose.longitude
+        if altitude is None:
+            altitude = self.__last_command_global_pose.altitude
+        new_pose.pose.position.latitude = latitude
+        new_pose.pose.position.longitude = longitude
+        new_pose.pose.position.altitude = altitude
+        if yaw is None:
+            new_pose.pose.orientation = self.__last_command_global_pose.pose.orientation
+        else:
+            q = quaternion_from_euler(0, 0, yaw)
+            new_pose.pose.orientation.x = q[0]
+            new_pose.pose.orientation.y = q[1]
+            new_pose.pose.orientation.z = q[2]
+            new_pose.pose.orientation.w = q[3]
+        print(new_pose.pose.orientation)
+        self.__yaw_head_first = yaw_head_first
+        self.__last_command_global_pose = new_pose
+        self.__type_of_move = 'GLOBAL_POSE'
 
     # VELOCITY velocity methods
     def set_velocity(self, x: float = 0, y: float = 0, z: float = 0, yaw: float = 0):
@@ -185,11 +250,11 @@ class Drone_api:
                             'Please run \'<object_name>.start()\'')
         new_vel = Twist()
         if x is None:
-            x = self.__last_command_pose.pose.position.x
+            x = self.__last_command_local_pose.pose.position.x
         if y is None:
-            y = self.__last_command_pose.pose.position.y
+            y = self.__last_command_local_pose.pose.position.y
         if z is None:
-            z = self.__last_command_pose.pose.position.z
+            z = self.__last_command_local_pose.pose.position.z
         if yaw is None:
             yaw = self.__last_command_vel.angular.z
         new_vel.linear.x = x
