3d06353ef5170993b5d35296b4a01b69e42ff64d
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-04-18 17:34:11 +0300

    Improved aruco_bottom.py

--- python_ws/src/test_pkg/src/aruco_bottom.py
+++ python_ws/src/test_pkg/src/aruco_bottom.py
@@ -4,6 +4,7 @@
 #  Зависнуть у маркера
 # =====================
 
+from turtle import circle
 import numpy as np
 import rospy
 import cv2
@@ -16,6 +17,44 @@ from math import pi, sqrt, sin, cos, atan2
 
 
 FONT = cv2.FONT_HERSHEY_PLAIN
+
+
+class Circle:
+    def __init__(self):
+        self.__buffer = [None] * 25
+        self.__yaw = 0
+        self.__counter = 0
+
+    def write(self, pose):
+        self.__buffer[self.__counter] = pose
+        self.__yaw = pose[5]
+        self.__counter += 1
+        if self.__counter >= len(self.__buffer):
+            self.__counter = 0
+
+    def mean(self):
+        num = 0
+        mean_pose = [0] * 6
+        for i in self.__buffer:
+            if i is not None:
+                num += 1
+                for j in range(len(mean_pose)):
+                    mean_pose[j] += i[j]
+        if num != 0:
+            for i in range(len(mean_pose) - 1):
+                mean_pose[i] = mean_pose[i] / num
+            mean_pose[5] = self.__yaw
+            """ if self.__buffer[self.__counter % len(self.__buffer)] is not None:
+                print(self.__buffer[self.__counter % len(self.__buffer)][5])
+                mean_pose[5] = self.__buffer[self.__counter %
+                                             len(self.__buffer)][5] """
+        return mean_pose
+
+    def erase_yaw(self):
+        self.__yaw = 0
+        """ for i in range(len(self.__buffer)):
+            if self.__buffer[i] is not None:
+                self.__buffer[i][5] = 0 """
 
 
 def toFixed(numObj, digits=0):
@@ -36,7 +75,7 @@ def callback(data):
                                                           parameters=parameters,
                                                           cameraMatrix=camera_matrix,
                                                           distCoeff=dist_coef)
-    global marker_pose
+    global cir
     if np.all(ids is not None):
         rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners, 0.2, camera_matrix,
                                                                    dist_coef)
@@ -51,7 +90,8 @@ def callback(data):
         drone_pose = drone.get_local_pose()
         x, y, z, roll, pitch, yaw = Camera_api.marker_local_pose(rvec[0][0], tvec[0][0],
                                                                  drone_pose, (0, 0, -0.05, 0, pi/2, 0))
-        marker_pose = [x, y, z, roll, pitch, yaw]
+        #marker_pose = [round(x, 1), round(y, 1), z, roll, pitch, yaw]
+        cir.write([x, y, z, roll, pitch, yaw])
         # Белая обводка и черный текст
         cv2.putText(frame, str(toFixed(x, 3)+'    ' +
                                toFixed(y, 3) + '    ' +
@@ -71,7 +111,8 @@ def callback(data):
                     FONT, 1, (0, 0, 0), 1, cv2.LINE_AA)
     else:
         # Сбрасываем yaw маркера, чтобы, в случае потери маркера, дрон не продолжал кружиться
-        marker_pose[5] = 0
+        #marker_pose[5] = 0
+        cir.erase_yaw()
         cv2.putText(frame, 'NOT FOUND', (20, 30), FONT,
                     1, (255, 255, 255), 3, cv2.LINE_AA)
         cv2.putText(frame, 'NOT FOUND', (20, 30), FONT,
@@ -87,7 +128,8 @@ camera_matrix, dist_coef = clb.loadCoefficients('calibration_save.yaml')
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters = aruco.DetectorParameters_create()
 
-marker_pose = [0, 0, 0, 0, 0, 0]
+cir = Circle()
+#marker_pose = [0, 0, 0, 0, 0, 0]
 drone = Drone_api()
 drone.start()
 rospy.loginfo('Drone armed')
@@ -104,16 +146,14 @@ while not drone.point_is_reached() and not drone.is_shutdown():
     drone.sleep(0.5)
 # Т.к. после закрытия топика с изображением сохраняется последний кадр,
 # необходимо перед стартом основной части скрипта "сбросить" значения маркера
-marker_pose = [0, 0, 0, 0, 0, 0]
-current_drone_x = 0
-current_drone_y = 0
+#marker_pose = [0, 0, 0, 0, 0, 0]
+cir = Circle()
 while not drone.is_shutdown():
+    marker_pose = cir.mean()
     drone_pose = drone.get_local_pose()
-    # Чтобы исключить "дерганье" дрона из-за небольшой ошибки определения местоположения маркера:
-    current_drone_x = (current_drone_x + marker_pose[0])/2
-    current_drone_y = (current_drone_y + marker_pose[1])/2
     correct_drone_yaw = marker_pose[5] + drone_pose[5]
-    drone.set_local_pose(current_drone_x, current_drone_y,
+    # Чтобы исключить "дерганье" дрона из-за небольшой ошибки определения местоположения маркера:
+    drone.set_local_pose(marker_pose[0], marker_pose[1],
                          marker_pose[2] + 4, correct_drone_yaw)
     drone.sleep(0.5)
 rospy.loginfo('Drone disarmed')
