1e3b3e983b77fe0787fb2537acb9a49babdbd672
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-04-27 16:02:31 +0300

    aruco_test.py ready

--- python_ws/src/real/src/aruco_test.py
+++ python_ws/src/real/src/aruco_test.py
@@ -11,21 +11,20 @@ import time
 from aruco_calibration import Calibration as clb
 import argparse
 
-# VIDEO SETTINGS
-fps = 30.0
-image_size = (620, 480)
-
 # ARGPARSER
 parser = argparse.ArgumentParser()
 parser.add_argument('--write', dest='write', action='store_true',
                     help='if set, video stream is written to a file')
 parser.add_argument('--show', dest='show', action='store_true',
                     help='if set, video stream is displayed in the window')
+parser.add_argument('--output', dest='output', action='store_true',
+                    help='if set, ArUco recognition process is output to the terminal')
 parser.add_argument('--no-ros', dest='no_ros', action='store_true',
                     help='if set, video stream is displayed in the window')
 args = parser.parse_args()
 parser.set_defaults(write=False)
 parser.set_defaults(show=False)
+parser.set_defaults(output=False)
 parser.set_defaults(no_ros=False)
 
 if not args.no_ros:
@@ -46,17 +45,19 @@ aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters = aruco.DetectorParameters_create()
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, image_size[0])
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size[1])
-cap.set(cv2.CAP_PROP_FPS, fps)
-if args.write:
-    out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'MJPG'),
-                          fps, image_size)
-
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+if args.write:
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            image_size = frame.shape[:2]
+            print(f'Resolution: {image_size[0]}x{image_size[1]}')
+            break
+    fps = 25.0
+    out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'MJPG'),
+                          fps, (image_size[1], image_size[0]))
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -67,8 +68,8 @@ while True:
                                                           parameters=parameters,
                                                           cameraMatrix=camera_matrix,
                                                           distCoeff=dist_coef)
-
-    print('\n\n\n')
+    if args.output:
+        print('\n\n\n')
     if np.all(ids is not None):
         rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners, 0.048, camera_matrix,
                                                                    dist_coef)
@@ -85,9 +86,9 @@ while True:
             x, y, z, roll, pitch, yaw = Camera_api.marker_cam_pose(rvec[0][0],
                                                                    tvec[0][0])
         marker_pose = [x, y, z, roll, pitch, yaw]
-        # Белая обводка и черный текст
-        print(toFixed(x, 3), toFixed(y, 3), toFixed(z, 3))
-        print(toFixed(roll, 3), toFixed(pitch, 3), toFixed(yaw, 3))
+        if args.output:
+            print(toFixed(x, 3), toFixed(y, 3), toFixed(z, 3))
+            print(toFixed(roll, 3), toFixed(pitch, 3), toFixed(yaw, 3))
         cv2.putText(frame, str(toFixed(x, 3)+'    ' +
                                toFixed(y, 3) + '    ' +
                                toFixed(z, 3) + '    '), (20, 90),
@@ -105,16 +106,18 @@ while True:
                                toFixed(yaw, 3)), (20, 120),
                     FONT, 1, (0, 0, 0), 1, cv2.LINE_AA)
     else:
-        print('NOT FOUND')
+        if args.output:
+            print('NOT FOUND')
         cv2.putText(frame, 'NOT FOUND', (20, 30), FONT,
                     1, (255, 255, 255), 3, cv2.LINE_AA)
         cv2.putText(frame, 'NOT FOUND', (20, 30), FONT,
                     1, (0, 0, 0), 1, cv2.LINE_AA)
     if args.write:
         out.write(frame)
-    # Для отладки. На headless-дистрибутивах крашится
+    # На headless-дистрибутивах крашится
     if args.show:
         cv2.imshow('frame', frame)
+        cv2.waitKey(1)
 cap.release()
 if args.write:
     out.release()
