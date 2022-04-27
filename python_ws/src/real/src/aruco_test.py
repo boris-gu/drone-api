a5eda6fb1b0774fe0679dd8ad4ac6aba2bb8737a
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-04-27 15:18:14 +0300

    Some fix aruco_test.py

--- python_ws/src/real/src/aruco_test.py
+++ python_ws/src/real/src/aruco_test.py
@@ -9,19 +9,37 @@ import cv2
 import cv2.aruco as aruco
 import time
 from aruco_calibration import Calibration as clb
-from drone_api import *  # закомментировать для тестирования без ROS
+import argparse
 
+# VIDEO SETTINGS
 fps = 30.0
-image_size = (848, 480)
-time_now = time.gmtime(time.time())
-video_file = f'{time.strftime("%Y.%m.%d %H:%M:%S", time.gmtime())}.avi'
+image_size = (620, 480)
 
-FONT = cv2.FONT_HERSHEY_PLAIN
+# ARGPARSER
+parser = argparse.ArgumentParser()
+parser.add_argument('--write', dest='write', action='store_true',
+                    help='if set, video stream is written to a file')
+parser.add_argument('--show', dest='show', action='store_true',
+                    help='if set, video stream is displayed in the window')
+parser.add_argument('--no-ros', dest='no_ros', action='store_true',
+                    help='if set, video stream is displayed in the window')
+args = parser.parse_args()
+parser.set_defaults(write=False)
+parser.set_defaults(show=False)
+parser.set_defaults(no_ros=False)
+
+if not args.no_ros:
+    from drone_api import *  # закомментировать для тестирования без ROS
 
 
 def toFixed(numObj, digits=0):
     return f'{numObj:.{digits}f}'
 
+
+time_now = time.gmtime(time.time())
+video_file = f'{time.strftime("%Y.%m.%d %H:%M:%S", time.gmtime())}.avi'
+
+FONT = cv2.FONT_HERSHEY_PLAIN
 
 camera_matrix, dist_coef = clb.loadCoefficients('calibration_save.yaml')
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
@@ -29,12 +47,12 @@ parameters = aruco.DetectorParameters_create()
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-# cap.set(cv2.CAP_PROP_EXPOSURE, 0.5) # работает не везде
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, image_size[0])
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size[1])
 cap.set(cv2.CAP_PROP_FPS, fps)
-out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'MJPG'),
-                      fps, image_size)
+if args.write():
+    out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc(*'MJPG'),
+                          fps, image_size)
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -61,10 +79,11 @@ while True:
                     1, (255, 255, 255), 3, cv2.LINE_AA)
         cv2.putText(frame, ' id' + str(ids[0])[1:-1], (20, 30), FONT,
                     1, (0, 0, 0), 1, cv2.LINE_AA)
-
-        x, y, z, roll, pitch, yaw = Camera_api.marker_cam_pose(rvec[0][0],
-                                                               tvec[0][0])
-        # x, y, z, roll, pitch, yaw = (1, 2, 3, 4, 5, 6)  # для отладки без ROS
+        if args.no_ros:
+            x, y, z, roll, pitch, yaw = (1, 2, 3, 4, 5, 6)
+        else:
+            x, y, z, roll, pitch, yaw = Camera_api.marker_cam_pose(rvec[0][0],
+                                                                   tvec[0][0])
         marker_pose = [x, y, z, roll, pitch, yaw]
         # Белая обводка и черный текст
         print(toFixed(x, 3), toFixed(y, 3), toFixed(z, 3))
@@ -91,10 +110,12 @@ while True:
                     1, (255, 255, 255), 3, cv2.LINE_AA)
         cv2.putText(frame, 'NOT FOUND', (20, 30), FONT,
                     1, (0, 0, 0), 1, cv2.LINE_AA)
-
-    out.write(frame)
-    # cv2.imshow('frame', frame) # для отладки. На headless-дистрибутивах крашится
-    # if cv2.waitKey(1) != -1:
-    #    break
+    if args.write:
+        out.write(frame)
+    # Для отладки. На headless-дистрибутивах крашится
+    if args.show:
+        cv2.imshow('frame', frame)
 cap.release()
+if args.write:
+    out.release()
 cv2.destroyAllWindows()
